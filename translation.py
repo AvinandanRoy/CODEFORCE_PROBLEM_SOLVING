945b1253207f218eedfbe2cf1293a9eaa735d51c
AvinandanRoy <abhinandanroylipi@gmail.com>
2024-02-26 00:31:33 +0600

    Translation is Ended and solve

--- translation.py
+++ translation.py
@@ -2,4 +2,14 @@
 # [start : End : Step]
 # [::-1] ----> for reversing
 
-firstLine
+firstLine = input()
+seconLine = input()
+
+# now i reverse the firstline value , then compare with secondline
+firstLinePrime = firstLine[::-1]
+
+# now i throw condition
+if firstLinePrime == seconLine:
+    print("YES")
+else:
+    print("NO")
